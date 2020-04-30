68352ddb0b69179f66379bfcabbb1f5771e58228
Pavel Nikolaev <pnikolaev859@gmail.com>
2020-04-30 20:25:27 +0300

    init

---
+++ code/lessons.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default ({ lessons }) => (
+    <ul className="collection">
+        <li className="collection-item avatar">
+            
+        </li>
+    </ul>
+);
---
+++ site/assets/js/sidenav.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const elems = document.querySelectorAll('.sidenav');
+    const instances = M.Sidenav.init(elems, options)
+});
